2a485d8919dc8e9b1410b1cecf1b46eaeeefb34f
Christian Moscardi <christian@flutes.localdomain>
2012-02-24 15:03:40 -0500

    2nd

--- ml_class_login.py
+++ ml_class_login.py
@@ -1,6 +1,8 @@
 import cookielib
 import urllib
 import urllib2
+import re
+from sys import argv
 from sys import exit
 
 from BeautifulSoup import BeautifulSoup
@@ -12,24 +14,25 @@ def get_login (username, password):
     ''' log in to what.cd and get a cookie
         opener is returned and should be used to open other pages inside '''
     
-    print 'logging in to what.cd as '+username
+    print('logging in as %s',username)
 
     cj = cookielib.CookieJar() 
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj)) 
     login_data = urllib.urlencode({'email_address': 'clm2183@columbia.edu',
                                    'password' : 'a54915'})
     opener.addheaders.append(('User-agent', 'Mozilla/4.0'))
-    wind= 'https://www.ml-class.org/course/auth/login'
+    page= 'https://www.ml-class.org/conurse/auth/login'
 
     #Try to log in and get the result html
-    opener.open(wind, login_data)
+    opener.open(page, login_data)
     check=opener.open('http://www.ml-class.org/course/class/index')
     print 'We are in.'
     return opener
 
-ml_opener=get_login("","")
+def get_vid_ids(page):
+    re.search('
+    
+ml_opener=get_login(argv[1],argv[2])
 page=ml_opener.open('http://www.ml-class.org/course/video/list?mode=download')
-print page.read()
-soup = BeautifulSoup(page.read())
 
-print soup.find('var', 'sections')
+
